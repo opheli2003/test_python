78039d184bff2f9d49c7a0cf73f18aaae0d75994
Gladys <gladys.ni@yahoo.fr>
2022-08-12 11:58:35 +0200

    exos

--- exercices.py
+++ exercices.py
@@ -3,10 +3,13 @@
 ########################################################################################
 
 
+import math
+
+
 class Ship:
     def __init__(self, name, size, author):
         self.name = name
-        self.size = size  # la size est un str qui vaut soir "small" soit "large"
+        self.size = size  # la size est un str qui vaut soit "small" soit "large"
         self.author = author
 
     def is_small(self):
@@ -39,6 +42,13 @@ class Spaceport:
 
 def is_same_ship(ship1, ship2):
     """ exercice 1 question 1 """
+    if ship1.size == ship2.size:
+        if ship1.name == ship2.name:
+            if ship1.author == ship2.author:
+                return True
+    else:
+        return False
+        
     raise NotImplementedError()
 
 
@@ -49,12 +59,27 @@ def is_same_ship(ship1, ship2):
 
 
 def filter_letters(string):
+    odd_chars = []
+    for i in range(len(string)):
+        if i % 2 != 0:
+            odd_chars.append(string[i])
+            return odd_chars
+
     """ exercice 2 question 1 """
     raise NotImplementedError()
 
 
 def count_letters(string):
     """ exercice 2 question 2 """
+    char_counter_dict = {}
+
+    for char in "string":
+        num_of_occur = 0
+        char_counter_dict[char] = num_of_occur
+        string.count(char)
+        # assign a counter of occurences for every character
+        # and put this counter as a value instead of 0 in num_of_occur
+
     raise NotImplementedError()
 
 
@@ -64,10 +89,14 @@ def mask_letters(string, mask):
 
 
 def puissances(start, end):
+    for i in range(start,end + 1):
+        return math.isqrt(i)**2 == i
+        
     """ exercice 2 question 4 """
+
     raise NotImplementedError()
 
 
 def return_int():
     """ exercice 2 question 5 """
-    return 42
+    return str(42)
